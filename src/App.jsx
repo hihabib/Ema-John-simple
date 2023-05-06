233498a51f96ab87e79f946571c3d73ea11779f0
Habibul Islam <habibulislam6862@gmail.com>
2023-05-06 16:25:28 +0600

    cart calculation added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,13 @@ import Header from "./components/Header/Header";
 import classes from "./App.module.css";
 import Shop from "./components/Shop/Shop";
 import OrderSummery from "./components/OrderSummery/OrderSummery";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 const App = () => {
   const [selectedProduct, setSelectedProduct] = useState([]);
   const handleProductSelection = (product) => {
     const newProductList = [...selectedProduct, product];
     setSelectedProduct(newProductList);
   };
-
-  useEffect(() => {
-    console.log(selectedProduct);
-  }, [selectedProduct]);
   return (
     <>
       <Header />
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,24 @@
-export default function Cart({selectedProduct}) {
-  const totalPrice = selectedProduct.reduce((acc, next) => {
-    acc = acc + next.price;
-    return acc;
-  }, 0);
+const TAX = 7;
+export default function Cart({ selectedProduct }) {
+  const [totalProductPrice, totalShippingCost] = selectedProduct.reduce(
+    (acc, next) => {
+      acc[0] = acc[0] + next.price;
+      acc[1] = acc[1] + next.shipping;
+
+      return acc;
+    },
+    [0, 0]
+  );
+  const totalTax = Number((totalProductPrice * (TAX / 100)).toFixed(2));
+  const grandTotal = totalProductPrice + totalShippingCost + totalTax;
+
   return (
     <div className="information">
       <div>Selected Item: {selectedProduct.length}</div>
-      <div>Total Price: ${totalPrice}</div>
-      <div>Total Shopping Charge: 0</div>
-      <div>Tax: 0</div>
-      <h3>Grand Total: 0</h3>
+      <div>Total Price: ${totalProductPrice.toFixed(2)}</div>
+      <div>Total Shopping Charge: ${totalShippingCost.toFixed(2)}</div>
+      <div>Tax: ${totalTax.toFixed(2)}</div>
+      <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
     </div>
   );
 }
